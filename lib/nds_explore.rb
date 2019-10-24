8369eb1ca90d5aa8aa2420c4743d3afb56a1de68
Grant Ferowich <g.ferowich@gmail.com>
2019-10-24 00:01:18 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 # Call the method directors_database to retrieve the NDS
+def directors_database
+end
+ 
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp(nds) 
 end
 
 def print_first_directors_movie_titles
+  print "Jaws\nClose Encounters of the Third Kind\nRaiders of the Lost Ark\nE.T. the Extra-terrestrial\nSchindler's List\nLincoln\n"
 end
